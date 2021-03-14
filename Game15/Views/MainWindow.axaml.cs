8bd1e0c9ef5cc3618d45ca7d6ce2c1befda02adf
Marat Nasibyan Ashoti <marat_17.5@live.com>
2021-03-14 21:44:30 +0400

    Add logic

--- Game15/Views/MainWindow.axaml.cs
+++ Game15/Views/MainWindow.axaml.cs
@@ -1,11 +1,14 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace Game15.Views
 {
     public class MainWindow : Window
     {
+        Game game;
         public MainWindow()
         {
             InitializeComponent();
@@ -17,6 +20,32 @@ namespace Game15.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            game = new Game(4);
+            game.Start();
+            Refresh();
+        }
+
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {       
+            var name = ((Button)sender).Name?.ToString();
+            Button button = this.FindControl<Button>(name);
+            game.Shift(int.Parse(name));
+            Refresh();
+            //button.Content = button.Name;
+        }
+
+      
+        private void Refresh()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+
+                Button button = this.FindControl<Button>(i.ToString());
+
+                button.Content = game.GetNumber(i).ToString();
+                button.IsVisible = game.GetNumber(i) > 0;
+            }
         }
     }
 }
