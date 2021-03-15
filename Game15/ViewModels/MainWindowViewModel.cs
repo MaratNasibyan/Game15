e71621afc873cee8bb83c52a0f7638d9ab8acb12
Marat Nasibyan Ashoti <marat_17.5@live.com>
2021-03-15 17:22:15 +0400

    commit

--- Game15/ViewModels/MainWindowViewModel.cs
+++ Game15/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Game15.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        private async Task InitializeAsync()
+        {           
+            await LoadData(4);
+        }
+
+        public async Task LoadData(int size)
+        {
+            //var data = await _movieService.GetMovies(page);
+            //await Task.Delay(1000);
+            //MyItems = new ObservableCollection<Movie>(data);
+        }
     }
 }
