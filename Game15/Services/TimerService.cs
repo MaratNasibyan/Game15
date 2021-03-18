7b242b51afa1cf13dd6cbd71792542774c2a8296
Marat Nasibyan Ashoti <marat_17.5@live.com>
2021-03-18 16:07:14 +0400

    add timer

---
+++ Game15/Services/TimerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game15.Services
+{
+    public class TimerService
+    {
+        Stopwatch timer = new Stopwatch();
+
+        public TimerService()
+        {
+            //      timer.Start();
+
+            ////B: Run stuff you want timed
+            //timer.Stop();
+
+            //TimeSpan timeTaken = timer.Elapsed;
+            //      string foo = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
--- Game15/ViewModels/MainWindowViewModel.cs
+++ Game15/ViewModels/MainWindowViewModel.cs
@@ -1,3 +1,5 @@
+using Avalonia.Threading;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,12 +9,28 @@ using System.Threading.Tasks;
 namespace Game15.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting => "Welcome to Avalonia!";
+    {      
+        private int timer = default;
+        private TimeSpan timeSpan;
+
+        public TimeSpan TimeSpan
+        {
+            get => timeSpan;
+            set => this.RaiseAndSetIfChanged(ref timeSpan, value);
+        }
+
 
         public MainWindowViewModel()
         {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
 
+        public void dispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            TimeSpan = TimeSpan.FromSeconds(timer++);
         }
 
         private async Task InitializeAsync()
